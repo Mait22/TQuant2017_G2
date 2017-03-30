1401c7009bc20226ca9d675e5f2f0797322bd184
BaumWolke <dominik.frommeyer@gmx.de>
2017-03-30 22:54:18 +0200

    Update Paper_binomial.R

--- Paper_binomial.R
+++ Paper_binomial.R
@@ -29,7 +29,7 @@ t <- seq(0.1, 8.1, by=2)
 yt <- rbinom(5, 50, .8)
 yt <- yt[order(yt, decreasing=TRUE)]
 n <- 50
-a <- 0.8
+a <- 0.4
 b <- 2
 c <- .4
 
@@ -75,9 +75,7 @@ loglike.1 <- function(param, data, t){
 # }
 
 loglike.2 <- function(param, data, t){
-  prob  <- sapply(t, 
-                  function(x){
-                    m.2(param, x)})
+  prob  <- m.2(param, t)
   prob <- c(prob, 1-prob) 
   #c((param[2] + t)^-param[1], 1-(param[2] + t)^-param[1])
   -sum(sapply(data, 
@@ -119,10 +117,9 @@ max.1 <- nlm(loglike.1, data=c(yi.1, n-yi.1), t=t, p=c(0.8))
 #       loglike.1(x, data=c(yi.1, n-yi.1), t=t)})
 # plot(aa,faa,type="l")
 
-max.2 <- nlm(loglike.2, data=c(yi.2, n-yi.2), t=t, p=c(.8, 0.99),
-             iterlim=1000)
+max.2 <- nlm(loglike.2, data=c(yi.2, n-yi.2), t=t, p=c(.8, 0.99))
 
-max.3 <- nlm(loglike.3, data=c(yi.3, n-yi.3), t=t, p=c(0.8, 0.5))
+max.3 <- nlm(loglike.3, data=c(yi.3, n-yi.3), t=t, p=c(0.5, 0.6))
 
 max.b <- nlm(loglike.b, data=c(yi.b, n-yi.b), t=t, p=c(0.8, 0.99, 0.2))
 
@@ -133,23 +130,33 @@ dat.1 <- data.frame(ID=seq_len(dat.n),
 dat.1$prob.m1 <- sapply(dat.1$time, 
                         function(x){
                           m.1(max.1$estimate, x)})
-dat.1$prob.m2 <- sapply(dat.1$time,
-                        function(x){
-                          m.2(max.2$estimate, x)})
-dat.1$prob.m3 <- sapply(dat.1$time,
-                        function(x){
-                          m.3(max.3$estimate, x)})
-dat.1$prob.mb <- sapply(dat.1$time,
-                        function(x){
-                          m.b(max.b$estimate, x)})
-
-dat.1$m_distr <- n*dat.1$prob.m1
 
 dat.1$frq_obs.m1 <- sapply(dat.1$prob.m1, 
                            function(x){
                              rbinom(1, n, x * error)
                            })
 dat.1$frq_nobs.m1 <- n - dat.1$frq_obs.m1
+dat.1$m_distr <- n*dat.1$prob.m1
+max.2.n <- nlm(loglike.2, 
+               data=c(dat.1$frq_obs.m1, dat.1$frq_nobs.m1),
+               t=dat.1$time, p=c(.8, 0.99))
+
+dat.1$prob.m2 <- sapply(dat.1$time,
+                        function(x){
+                          m.2(max.2.n$estimate, x)})
+
+max.3.n <- nlm(loglike.3, 
+               data=c(dat.1$frq_obs.m1, dat.1$frq_nobs.m1),
+               t=dat.1$time, p=c(.8, 0.99))
+dat.1$prob.m3 <- sapply(dat.1$time,
+                        function(x){
+                          m.3(max.3.n$estimate, x)})
+max.b.n <- nlm(loglike.b, 
+               data=c(dat.1$frq_obs.m1, dat.1$frq_nobs.m1),
+               t=dat.1$time, p=c(0.8, 0.99, 0.2))
+dat.1$prob.mb <- sapply(dat.1$time,
+                        function(x){
+                          m.b(max.b.n$estimate, x)})
 
 dat.1$frq_prd.m1 <- sapply(dat.1$prob.m1, 
                            function(x){
@@ -182,32 +189,35 @@ dat.1$SE.m2 <- (dat.1$frq_obs.m1 - dat.1$frq_prd.m2)^2
 dat.1$SE.m3 <- (dat.1$frq_obs.m1 - dat.1$frq_prd.m3)^2
 dat.1$SE.mb <- (dat.1$frq_obs.m1 - dat.1$frq_prd.mb)^2
 
-
-SSE.m1.1 <- sum(dat.1$SE.m1)
-SSE.m2.1 <- sum(dat.1$SE.m2)
-SSE.m3.1 <- sum(dat.1$SE.m3)
-SSE.mb.1 <- sum(dat.1$SE.mb)
-
-RMSE.m1.1 <- sqrt(SSE.m1.1/length(dat.1$frq_obs.m1))
-RMSE.m2.1 <- sqrt(SSE.m2.1/length(dat.1$frq_obs.m1))
-RMSE.m3.1 <- sqrt(SSE.m3.1/length(dat.1$frq_obs.m1))
-RMSE.mb.1 <- sqrt(SSE.mb.1/length(dat.1$frq_obs.m1))
+RMSE.1 <- data.frame(Model=c(1,2,3, "b"),
+                   SSE=c( sum(dat.1$SE.m1),
+                          sum(dat.1$SE.m2),
+                          sum(dat.1$SE.m3),
+                          sum(dat.1$SE.mb)),
+                   RMSE=c( sqrt(SSE.m1.1/length(dat.1$frq_obs.m1)),
+                           sqrt(SSE.m2.1/length(dat.1$frq_obs.m1)),
+                           sqrt(SSE.m3.1/length(dat.1$frq_obs.m1)),
+                           sqrt(SSE.mb.1/length(dat.1$frq_obs.m1))),
+                   SST=rep(sum((dat.1$frq_obs.m1 - 
+                                  mean(dat.1$frq_obs.m1))^2), 4))
 
 
 # #SST.m1.1 <- sum((dat.1$frq_obs - dat.1$m_distr)^2) 
 # # ?, seems strange.
 # #
-# # What about (yep, this is the way to go)
-SST.m1.1 <- sum((dat.1$frq_obs.m1 - mean(dat.1$frq_obs.m1))^2)
-SST.m2.1 <- sum((dat.1$frq_obs.m1 - mean(dat.1$frq_obs.m1))^2)
-SST.m3.1 <- sum((dat.1$frq_obs.m1 - mean(dat.1$frq_obs.m1))^2)
-SST.mb.1 <- sum((dat.1$frq_obs.m1 - mean(dat.1$frq_obs.m1))^2)
+# # What about (yep, this is the way to go, Myung,)
+#SST.m1.1 <- sum((dat.1$frq_obs.m1 - mean(dat.1$frq_obs.m1))^2)
+#SST.m2.1 <- sum((dat.1$frq_obs.m1 - mean(dat.1$frq_obs.m1))^2)
+#SST.m3.1 <- sum((dat.1$frq_obs.m1 - mean(dat.1$frq_obs.m1))^2)
+#SST.mb.1 <- sum((dat.1$frq_obs.m1 - mean(dat.1$frq_obs.m1))^2)
 
 #PVAF:
-PVAF.m1.1 <- (1 - (SSE.m1.1/SST.m1.1))
-PVAF.m2.1 <- (1 - (SSE.m2.1/SST.m2.1))
-PVAF.m3.1 <- (1 - (SSE.m3.1/SST.m3.1))
-PVAF.mb.1 <- (1 - (SSE.mb.1/SST.mb.1))
+PVAF.1 <- data.frame(Model=c(1,2,3,"b"),
+                     PVAF=c((1 - (SSE.m1.1/SST.m1.1)),
+                            (1 - (SSE.m2.1/SST.m2.1)),
+                            (1 - (SSE.m3.1/SST.m3.1)),
+                            (1 - (SSE.mb.1/SST.mb.1))))
+
 
 ## AIC, BIC
 b.aic <- function(loglike, nparam){
@@ -217,29 +227,38 @@ b.bic <- function(loglike, nparam, sample){
   -2*loglike + nparam*log(sample)
 }
 
-m1.1.aic <- b.aic(loglike.1(param=max.1$estimate, 
-                            data=c(dat.1$frq_obs.m1,
-                                   dat.1$frq_nobs.m1),
-                            t=rep(dat.1$time, by=2)),
-                  length(max.1$estimate))
 
-m1.1.bic <- b.bic(loglike.1(param=max.1$estimate, 
-                            data=c(dat.1$frq_obs.m1,
-                                   dat.1$frq_nobs.m1),
-                            t=rep(dat.1$time, 2)),
-                  length(max.1$estimate),
-                  length(dat.1$frq_obs.m1))
-
-
-m3.1.aic <- b.aic(loglike.3(param=max.3$estimate,
-                            data=c(dat.1$frq_obs.m1,
-                                   dat.1$frq_nobs.m1),
-                            t=rep(dat.1$time, by=2)),
-                  length(max.3$estimate))
-m3.1.bic <- b.bic(loglike.3(param=max.3$estimate,
-                            data=c(dat.1$frq_obs.m1,
-                                   dat.1$frq_nobs.m1),
-                            t=rep(dat.1$time, by=2)),
-                  length(max.3$estimate),
-                  length(dat.1$frq_nobs.m1))
-
+aic.1 <- data.frame(Model=c(1,2,3),
+                    AIC=c(b.aic(loglike.1(param=max.1$estimate, 
+                                          data=c(dat.1$frq_obs.m1,
+                                                 dat.1$frq_nobs.m1),
+                                          t=dat.1$time),
+                                length(max.1$estimate)),
+                          b.aic(loglike.2(param=max.2$estimate, 
+                                          data=c(dat.1$frq_obs.m1,
+                                                 dat.1$frq_nobs.m1),
+                                          t=dat.1$time),
+                                length(max.2$estimate)),
+                          b.aic(loglike.3(param=max.3$estimate,
+                                          data=c(dat.1$frq_obs.m1,
+                                                 dat.1$frq_nobs.m1),
+                                          t=dat.1$time),
+                                length(max.3$estimate))),
+                    BIC=c(b.bic(loglike.1(param=max.1$estimate, 
+                                          data=c(dat.1$frq_obs.m1,
+                                                 dat.1$frq_nobs.m1),
+                                          t=dat.1$time),
+                                length(max.1$estimate),
+                                length(dat.1$frq_obs.m1)),
+                          b.bic(loglike.2(param=max.2$estimate, 
+                                          data=c(dat.1$frq_obs.m1,
+                                                 dat.1$frq_nobs.m1),
+                                          t=dat.1$time),
+                                length(max.2$estimate),
+                                length(dat.1$frq_obs.m1)),
+                          b.bic(loglike.3(param=max.3$estimate,
+                                          data=c(dat.1$frq_obs.m1,
+                                                 dat.1$frq_nobs.m1),
+                                          t=dat.1$time),
+                                length(max.3$estimate),
+                                length(dat.1$frq_nobs.m1))))
