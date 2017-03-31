7d78fd4071a8335b2807e3ab5c76023b71ebb9dd
BaumWolke <dominik.frommeyer@gmx.de>
2017-03-31 14:01:59 +0200

    Update AICBIC.R

--- AICBIC.R
+++ AICBIC.R
@@ -15,29 +15,29 @@ pvaf <- function(obs, pred){
 
 ## Example on how to use it,
 ## dat.1 ist teh dataframe in which frequencies and probabilities as well as t are stored
-liste <- list(list(max.1.n, loglike.1),
-              list(max.2.n, loglike.2),
-              list(max.3.n, loglike.3))
-
-aic.1 <- data.frame(Model=c(1,2,3),
-                    AIC=unlist(
-                      lapply(liste,
-                             function(x){
-                               b.aic(
-                                 x[[2]](param=x[[1]]$estimate,
-                                           data=c(dat.1$frq_obs.m1,
-                                           dat.1$frq_nobs.m1),
-                                           t=dat.1$time),
-                                 length(x[[1]]$estimate))})),
-                    BIC=unlist(
-                      lapply(liste,
-                             function(x){
-                               b.bic(
-                                 x[[2]](param=x[[1]]$estimate,
-                                        data=c(dat.1$frq_obs.m1,
-                                               dat.1$frq_nobs.m1),
-                                        t=dat.1$time),
-                                 length(x[[1]]$estimate),
-                                 length(dat.1$frq_obs.m1))
-                             })
-                    ))
+#liste <- list(list(max.1.n, loglike.1),
+#              list(max.2.n, loglike.2),
+#              list(max.3.n, loglike.3))
+#
+#aic.1 <- data.frame(Model=c(1,2,3),
+#                    AIC=unlist(
+#                      lapply(liste,
+#                             function(x){
+#                               b.aic(
+#                                 x[[2]](param=x[[1]]$estimate,
+#                                           data=c(dat.1$frq_obs.m1,
+#                                           dat.1$frq_nobs.m1),
+#                                           t=dat.1$time),
+#                                 length(x[[1]]$estimate))})),
+#                    BIC=unlist(
+#                      lapply(liste,
+#                             function(x){
+#                               b.bic(
+#                                 x[[2]](param=x[[1]]$estimate,
+#                                        data=c(dat.1$frq_obs.m1,
+#                                               dat.1$frq_nobs.m1),
+#                                        t=dat.1$time),
+#                                 length(x[[1]]$estimate),
+#                                 length(dat.1$frq_obs.m1))
+#                             })
+#                    ))
